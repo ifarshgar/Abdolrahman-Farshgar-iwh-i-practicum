17874815ccf905f0dda80746b46488b70b8da805
Abdolrahman Farshgar <rahman@Abdolrahmans-MacBook-Air.local>
2025-06-08 15:54:42 +0200

    Added required routes for Getting and Updating the custom object. However there are some bugs. I get 400 error :|

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
+const { HUBSPOT_AUTH_TOKEN } = require('./config');
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
@@ -8,19 +9,63 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = HUBSPOT_AUTH_TOKEN;
+const CUSTOM_OBJECT_TYPE = 'video_games';
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+// ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+app.get('/', async (req, res) => {
+  const apiUrl = `https://api.hubapi.com/crm/v3/objects/${CUSTOM_OBJECT_TYPE}`;
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json',
+  };
 
-// * Code for Route 1 goes here
+  try {
+    const response = await axios.get(apiUrl, { headers });
+    const data = response.data.results;
+    res.render('homepage', {
+      title: 'Custom Objects | HubSpot Practicum',
+      data,
+      objectType: CUSTOM_OBJECT_TYPE,
+    });
+  } catch (error) {
+    console.error(error);
+    res.render('homepage', {
+      title: 'Custom Objects | HubSpot Practicum',
+      error: 'Failed to load data',
+    });
+  }
+});
 
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
+// ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
+app.get('/update/video-game', (req, res) => {
+  res.render('updates', {
+    title: 'Update Custom Object | HubSpot Practicum',
+  });
+});
 
-// * Code for Route 2 goes here
+// ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
+app.post('/update/video-game', async (req, res) => {
+  const apiUrl = `https://api.hubapi.com/crm/v3/objects/${CUSTOM_OBJECT_TYPE}`;
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json',
+  };
 
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
+  const { name, genre, price } = req.body;
+  const newRecord = { properties: { name, genre, price } };
 
-// * Code for Route 3 goes here
+  try {
+    await axios.post(apiUrl, newRecord, { headers });
+    res.redirect('/');
+  } catch (error) {
+    console.error(error);
+    res.render('updates', {
+      title: 'Update Custom Object | HubSpot Practicum',
+      error: 'Failed to create record. Please try again.',
+    });
+  }
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
@@ -65,7 +110,6 @@ app.post('/update', async (req, res) => {
 
 });
 */
-
 
 // * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
